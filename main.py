4f8d446282aeb3f2bc926d13ceabc9f24088c3bc
MeetG <meet.g@hotmail.com>
2024-01-17 22:44:42 +0000

    Made function to load unemployment data of white individuals by borough. Output as 2d array of boroughs

--- main.py
+++ main.py
@@ -19,7 +19,29 @@ def load_employment(year):
             if borough == "Westminster":
                 break
             # Stop at westminister since this is the last borough
-        except:
+        except: 
             pass
     return boroughs, employed
+
+def load_unemployment_white_borough(year):
+    employment_rate = './ea-rate-and-er-by-eg-and-nation.xls'
+    df_2 = pd.read_excel(employment_rate, sheet_name=str(year), index_col=None, skiprows=2)
+    data = []
+    for i, j in df_2.iterrows():
+        try:
+            if j[1] == "City of London":
+                continue
+            if j[20] == "!":
+                pass
+            else:
+                percentEmployed = j[20]
+                borough = j[1]
+                data.append([borough,percentEmployed])
+                # append the borough and percentage employed
+            if borough == "Westminster":
+                break
+            # Stop at westminister since this is the last borough
+        except: 
+            pass
+    return data
 
